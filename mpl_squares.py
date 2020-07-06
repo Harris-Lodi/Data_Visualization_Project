c2f2697d1f3a20722d26e2afdd27055e1276c651
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-06 01:14:11 -0400

    Added Scatter Plot script to test scatter plots
    Also added a feature to save plot automatically rather then having it show it to us. Also added titles, axis labels, colors, new datasets, etc to original script from yesterday too!

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,13 +1,25 @@
 import matplotlib.pyplot as plt
 
 # data to visualize
-squares = [1,4,9,16,25]
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+# set style for the plot
+plt.style.use('seaborn')
 
 # assign fig and ax as asix for the plot
 fig, ax = plt.subplots()
 
-# plot the data to visualize
-ax.plot(squares)
+# plot the data to visualize, line width change to alter looks
+ax.plot(input_values, squares, linewidth = 3)
+
+# set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Input Values", fontsize=14)
+ax.set_ylabel("square of Value", fontsize=14)
+
+# set size of tick labels
+ax.tick_params(axis = 'both', labelsize = 14)
 
 # show plot
 plt.show()
---
+++ scatter_squares.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt # page 315
+
+# data set of 1000 dots too look like a line
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values] # for loop result is assigned to y_values
+
+plt.style.use('seaborn')
+
+fig, ax = plt.subplots()
+
+# x values, y values, size of dots, change colors too using color map
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s = 10)
+
+# set chart title and labels
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Input Values", fontsize=14)
+ax.set_ylabel("Square of Values", fontsize=14)
+
+# set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# set the range for each axis
+ax.axis([0, 1100, 0, 1100000])
+
+# save plots automatically, use plot.show() to show plot instead
+plt.savefig('squares_plot.png', bbox_inches = 'tight')
