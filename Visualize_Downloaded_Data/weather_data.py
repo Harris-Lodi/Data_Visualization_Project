46134c2f78a69c521f72c008749e00609deea612
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-10 21:56:41 -0400

    Finished the weather data reader
    Added a second method for reading the data that catches empty spaces and gives a custom error, rather then stopping the code and will continue to show the code!

--- Visualize_Downloaded_Data/weather_data.py
+++ Visualize_Downloaded_Data/weather_data.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt 
 from datetime import datetime
 
-# save data to filename variable, page 340
+# save data to filename variable, page 347
 # source: https://codinginfinite.com/data-visualizing-csv-format-chart-using-python-matplotlib/
 filename = 'data/sitka_weather_2018_full.csv'
 
@@ -13,26 +13,54 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    # get high temperatures from the file, and dates too
-    dates, highs = [], []
-    for row in reader:
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = row[8] # get values from row 8, high temps in file
-        dates.append(current_date)
-        highs.append(high)
-        highs[:] = [x for x in highs if x] # remove empty spots from list
-        inthighs = [int(x) for x in highs] # convert each items in highs to an int
+    # get high temperatures from the file, and dates too, and lows 
+    dates, highs, lows = [], [], []
 
-    # print(inthighs)
+    # region this is reader method 1 to avoid errors!
+
+    # for row in reader: 
+    #     current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    #     high = row[8] # get values from row 8, high temps in file
+    #     low = row[9]
+    #     dates.append(current_date)
+    #     highs.append(high)
+    #     lows.append(low)
+    #     highs[:] = [x for x in highs if x] # remove empty spots from list
+    #     inthighs = [int(x) for x in highs] # convert each items in highs to an int
+    #     lows[:] = [x for x in lows if x]
+    #     intlows = [int(x) for x in lows]
+    
+    #endregion
+
+    # region this is reader method 2 to avoid errors
+    for row in reader: 
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[8]) # get values from row 8, high temps in file
+            low = int(row[9])
+        except ValueError:
+            print(f"Missing Data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+    # endregion
+
+    # for debugging purposes, use inthighs and intlows when using reader method 1
+    # print(highs)
+    # print(lows)
     # print(dates)
 
-    # plot the high temperatures
+    # plot the high temperatures, swap 'highs' and 'lows' for
+    # 'inthighs' and 'intlows' if using reader method 1
     plt.style.use('seaborn')
     fig, ax, = plt.subplots()
-    ax.plot(dates, inthighs, c='red')
+    ax.plot(dates, highs, c='red', alpha = 0.5) # plot high temps vs dates, alpha is alpha albedo
+    ax.plot(dates, lows, c = 'blue', alpha = 0.5) # plot low temps vs dates
+    plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.1) # shade area between plot lines
 
     # format plot
-    plt.title('Daily high temps, Jan 1st 2018 to Dec 31st 2018', fontsize = 24)
+    plt.title('Daily high and low temps, Jan 1st 2018 to Dec 31st 2018', fontsize = 24)
     plt.xlabel('', fontsize = 16)
     fig.autofmt_xdate()
     plt.ylabel('Temperature (F)', fontsize = 16)
